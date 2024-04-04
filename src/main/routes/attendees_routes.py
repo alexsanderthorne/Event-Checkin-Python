f78455fcd313d646f7ffb83c559ae9cee6785887
alexsanderthorne <alexsanderthorne@gmail.com>
2024-04-04 10:22:00 -0300

    Implementing get attendees by eventId

--- src/main/routes/attendees_routes.py
+++ src/main/routes/attendees_routes.py
@@ -27,3 +27,15 @@ def get_attendees_batch(attendee_id):
     except Exception as exception:
         http_response = Exception(exception)
         return jsonify(http_response.body), http_response.status_code
+
+@attendees_route_bp.route("/events/<event_id>/attendees", methods=["GET"])
+def get_attendees(event_id):
+    try:
+        attendees_handle = AttendeesHandler()
+        http_request = HttpRequest(param={ "event_id": event_id })
+
+        http_response = attendees_handle.find_attendees_from_event(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = Exception(exception)
+        return jsonify(http_response.body), http_response.status_code
